7bdb255a4b4463e28ecf5ca2d3bbb001ef7e4eee
DanielSh02 <77538962+DanielSh02@users.noreply.github.com>
2022-06-29 20:42:17 +0800

    uses official wordle word list

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ def main():
             guess = input("Enter your guess: ")
             if wordle.isCorrect(guess):
                 print(f"Congrats! You won in {guesses} guesses!")
+                break
             else:
                 print(wordle.result(guess))
         except:
--- wordle.py
+++ wordle.py
@@ -1,10 +1,19 @@
+from email.policy import default
 import random
+
+
+default_word_list = set()
+with open("wordlists/valid-wordle-words.txt", "r") as words:
+    for word in words:
+        default_word_list.add(word.strip())
+
 
 class Wordle():
 
-    def __init__(self, guesses = 6) -> None:
-        self.words = ["apple", "simon", "chess", "table", "glass"]
-        self.answer = random.choice(self.words)
+    def __init__(self, words = default_word_list, guesses = 6) -> None:
+        self.words = words
+        self.answer = random.choice(list(self.words))
+        print(self.answer)
         self.guesses = guesses
     
     def isCorrect(self, word: str) -> bool:
@@ -12,7 +21,9 @@ class Wordle():
     
     def result(self, word: str) -> list[int]:
         word = word.lower()
-        if word not in self.words:
+        print(self.words)
+        print(word in self.words)
+        if not word in self.words:
             raise Exception("Invalid Guess")
         res = []
         for i, c in enumerate(word):
@@ -23,6 +34,8 @@ class Wordle():
             else:
                 res.append(0)
         return res
+
+
     
     
 
