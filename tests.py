8e695cd64804fcb1dee62112a7e44973faaff936
DanielSh02 <77538962+DanielSh02@users.noreply.github.com>
2022-07-01 00:05:01 +0800

    testing that avg number of guesses count is right

---
+++ tests.py
@@ -0,0 +1,30 @@
+words = random.sample(default_word_list, 50)
+bot = Bot(words=words, guesses=5)
+print(words)
+# print(bot.solve())
+
+wordlist = words[0:]
+
+def test_avg_guesses():
+    beta, best_word = bot.solve()
+
+    predicted_avg = beta / len(wordlist)
+    print(best_word, predicted_avg)
+    total_guessses = 0
+    for i, ans in enumerate(wordlist):
+        if not i % 10 and i > 0:
+            print(f'Completed batch {i}: avg_guesses = {total_guessses / i}')
+        depth = 1
+        bot.words = wordlist[0:]
+        print(f'Answer: {ans}')
+        bot.answer = ans
+        guess = best_word
+        while not bot.isCorrect(guess):
+            partition = bot.result(guess)
+            bot.words = [g for g in bot.words if bot.validHardmodeGuess(g, [guess], partitions=[partition])]
+            guess = bot.solve()[1]
+            depth += 1
+        total_guessses += depth
+    print(f'Predicted: {predicted_avg}, actual: {total_guessses / len(wordlist)}')
+
+test_avg_guesses()
