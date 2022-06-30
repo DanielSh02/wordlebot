aff4ebaef46f1cf465373f39a7c53c4dfb383e3c
DanielSh02 <77538962+DanielSh02@users.noreply.github.com>
2022-06-30 19:00:51 +0800

    added support for hardmode to wordle class

--- wordle.py
+++ wordle.py
@@ -1,4 +1,7 @@
+import enum
 import random
+
+from numpy import ndenumerate
 
 
 default_word_list = []
@@ -9,17 +12,34 @@ with open("wordlists/valid-wordle-words.txt", "r") as words:
 
 class Wordle():
 
-    def __init__(self, words = default_word_list, guesses = 6) -> None:
+    def __init__(self, words = default_word_list, guesses = 6, hardmode = True) -> None:
         self.words = words
         self.answer = random.choice(list(self.words))
         print(self.answer)
         self.guesses = guesses
+        self.hardmode = hardmode
     
     def isCorrect(self, word: str) -> bool:
         return word == self.answer
+
+    def validHardmodeGuess(self, prev_words, guess, partitions=None):
+        if not partitions:
+            partitions = [self.result(w) for w in prev_words]
+        for w in prev_words:
+            for i, c in enumerate(w):
+                if c == 2 and guess[i] != w[i]:
+                    return False
+                if c == 1 and (guess[i] == w[i] or w[i] not in guess):
+                    return False
+                if c == 0 and w[i] in guess:
+                    return False
+        return True 
     
-    def result(self, word: str, ans) -> list[int]:
+    def result(self, word: str, ans = None) -> list[int]:
+        if not ans:
+            ans = self.answer
         word = word.lower()
+        # TODO: Implement checking for valid guesses in hardmode.
         if not word in self.words:
             raise Exception("Invalid Guess")
         res = ""
