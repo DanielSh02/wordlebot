e9d80b92f9ee471e7543f9ecb31efd0e3a84ff74
DanielSh02 <77538962+DanielSh02@users.noreply.github.com>
2022-07-05 21:36:33 +0800

    added more tests

--- tests.py
+++ tests.py
@@ -1,16 +1,18 @@
+from email.policy import default
 import unittest
 from bot import *
 from wordle import *
+import time
 
-words = random.sample(default_word_list, 1000)
+words = random.sample(default_word_list, 500)
 bot = Bot(words=words, guesses=5)
 # print(words)
 
 wordlist = words[0:]
 
 def test_avg_guesses():
+    start = time.time()
     beta, best_word = bot.solve()
-
     predicted_avg = beta / len(wordlist)
     print(best_word, predicted_avg)
     total_guessses = 0
@@ -28,22 +30,47 @@ def test_avg_guesses():
             depth += 1
         total_guessses += depth
     print(f'Predicted: {predicted_avg}, actual: {total_guessses / len(wordlist)}')
-
-# test_avg_guesses()
+    end = time.time()
+    print(end - start)
 
 wordle = Wordle(hardmode = False)
-hard_wordle = Wordle()
+# bot = Bot()
 
 class WordleTests(unittest.TestCase):
     
     def test_result(self):
         self.assertEqual(wordle.result("crane", "place"), "10202")
         self.assertEqual(wordle.result("crane", "jazzy"), "00100")
-        self.assertEqual(wordle.result("tests", "jazzy"), "00000")
-        self.assertEqual(wordle.result("radar", "alarm"), "11010")
-        self.assertEqual(wordle.result("geese", "elite"), "01002")
+        self.assertEqual(wordle.result("tests", "jazzy"), "00000", "all 0s")
+        self.assertEqual(wordle.result("radar", "alarm"), "11010", "double letters")
+        self.assertEqual(wordle.result("geese", "elite"), "01002", "triple letters")
+        with self.assertRaises(Exception, msg="invalid guess exception"):
+            wordle.result("xxxxx", "crane")
+
+    def test_hardmode(self):
+        self.assertTrue(wordle.validHardmodeGuess("place", "crane", "10202"))
+
+    def test_gen_partitions(self):
+        possible_ans = ["crane",
+                        "train",
+                        "jazzy",
+                        "simon",
+                        "plane",
+                        "flout",
+                        "clout",
+                        "arise"]
+        self.assertDictEqual(bot.gen_partitions("arise", possible_ans), 
+                             {"12002": ["crane"], 
+                              "12100": ["train"],
+                              "10000": ["jazzy"],
+                              "00110": ["simon"],
+                              "10002": ["plane"],
+                              "00000": ["flout", "clout"],
+                              "22222": ["arise"]},
+                             msg = "gen_partitions med")
 
 
-if __name__ == '__main__':
-    unittest.main()
+# if __name__ == '__main__':
+#     unittest.main()
+test_avg_guesses()  
 
